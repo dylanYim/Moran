7bb186d168ef2772d73bc9927ac2f2c000463c0b
dongkyun <dongkyun.yim@brique.co.kr>
2022-04-10 01:59:24 -0700

    DailyCommit-002  - create Member Repository

--- src/main/java/dylan/moran/domain/member/Member.java
+++ src/main/java/dylan/moran/domain/member/Member.java
@@ -3,9 +3,16 @@ package dylan.moran.domain.member;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Getter @Setter
 public class Member {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String eMail;
     private String password;
